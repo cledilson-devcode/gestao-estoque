090d4afc0388ff52e81e3b8efa2d25c13cf5ec91
Cledilson <cledilsondevcod@gmail.com>
2024-08-13 22:32:33 -0300

    feat: Criação do metodo POST para salvar o produto.

--- src/main/java/com/cledilsondevcode/estoqueproduto/controllers/ProductController.java
+++ src/main/java/com/cledilsondevcode/estoqueproduto/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.cledilsondevcode.estoqueproduto.controllers;
 
+import com.cledilsondevcode.estoqueproduto.dtos.ProductRecordDto;
+import com.cledilsondevcode.estoqueproduto.models.ProductModel;
 import com.cledilsondevcode.estoqueproduto.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +17,11 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        ProductModel productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 }
